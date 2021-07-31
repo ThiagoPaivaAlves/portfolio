02bab3c0508f05ff9b279998e09fa52c835ae777
ThiagoPaivaAlves <88196831+ThiagoPaivaAlves@users.noreply.github.com>
2021-07-31 00:16:09 -0300

    fixed menu active links

--- routes/custom.js
+++ routes/custom.js
@@ -3,15 +3,15 @@ var router = express.Router();
 
 /* GET home about */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'A little bit about me', page:'about' });
+  res.render('about', { title: 'A little bit about me', about: true });
 });
 
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Bullet proof certified projects', page:'project' });
+  res.render('projects', { title: 'Bullet proof certified projects', projects: true });
 });
 
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact', page:'contact' });
+  res.render('contact', { title: 'Contact', contact: true });
 });
 
 module.exports = router;
